f8bcf09186efa4afd055fab41299de11ea18752f
Gerald <geraldmir4@gmail.com>
2022-06-29 16:08:19 +0800

    Add files needed for Heroku deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 const morgan = require('morgan')
 
 morgan.token('body', (request, response) => request.method === 'POST' ? JSON.stringify(request.body) : '')
